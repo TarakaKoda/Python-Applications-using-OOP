d18e529f674212c8eece90c6a5bbcdb3381ea277
TarakaKoda <taraka.koda@gmail.com>
2023-04-14 02:37:05 +0530

    class Phone (child class/subclass of Item)

---
+++ phone.py
@@ -0,0 +1,20 @@
+from item import Item
+
+
+class Phone(Item):
+    all = []
+
+    def __init__(self, name: str, price: float, quantity: int, broken_phone: int):
+        super().__init__(name, price, quantity)
+        # Run validations for received arguments
+        assert broken_phone >= 0, "Broken Phone is not greater than zero"
+
+        # Assigning arguments
+        self.broken_phone = broken_phone
+        Phone.all.append(self)
+
+    def condition_of_phone(self):
+        return f"{self.item_name}\nBroken Phones: {self.broken_phone}\nWorking Condition: {self.item_quantity - self.broken_phone}\n"
+
+    def __repr__(self):
+        return f'{self.__class__.__name__}("{self.item_name}", {self.item_price}, {self.item_quantity}, {self.broken_phone})'
